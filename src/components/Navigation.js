dfe526a937bb853e471a31bc120b55599ba04cbb
Julia Rodrigues <juliardgs@yahoo.com.br>
2020-08-01 17:14:24 -0300

    Made Navigation a component

---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import logo from "./../assets/logo1.png";
+import register from "./../assets/register.png";
+import trade from "./../assets/trade4.png";
+import location from "./../assets/loc1.png";
+import report from "./../assets/report2.png";
+import { Link } from "react-router-dom";
+import { Nav, NavItem } from "reactstrap";
+
+function Navigation() {
+  return (
+    <>
+      <Link to="/">
+        <img className="app-logo" src={logo} alt="Zombie Resident" />
+      </Link>
+      <Nav vertical pills className="pl-5 mt-3">
+        <NavItem className="mt-4">
+          <Link to="/register">
+            <img className="nav-icons" src={register} alt="Register img" />
+            Register
+          </Link>
+        </NavItem>
+        <NavItem className="mt-4">
+          <Link to="/updateLocation">
+            <img className="nav-icons" src={location} alt="Location img" />
+            Update Location
+          </Link>
+        </NavItem>
+        <NavItem className="mt-4">
+          <Link to="/trade">
+            <img className="nav-icons" src={trade} alt="Trade img" />
+            Trade
+          </Link>
+        </NavItem>
+        <NavItem className="mt-4">
+          <Link to="/reportInfected">
+            <img className="nav-icons" src={report} alt="Report img" />
+            Report
+          </Link>
+        </NavItem>
+      </Nav>
+    </>
+  );
+}
+
+export default Navigation;
