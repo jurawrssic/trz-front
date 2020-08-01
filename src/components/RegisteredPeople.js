ed87a137c5fd4f15d364b3ff7dc88c74e407ef40
Julia Rodrigues <juliardgs@yahoo.com.br>
2020-08-01 17:22:58 -0300

    Deleted unused files

--- src/components/RegisteredPeople.js
+++ src/components/RegisteredPeople.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { Table, Input } from "reactstrap";
+
 import axios from "axios";
+
 import ReportInfectedModal from "./ReportInfectedModal";
+import female from "./../assets/female.png";
+import male from "./../assets/male.png";
+import dead from "./../assets/infected.png";
 
 function PersonTable({ person, index }) {
   // Change style from in line, and disable button when infected
   person.id = person.location.split("/")[5];
+  const tableStyle = {
+    textDecoration: person.infected ? "line-through" : "none",
+    color: person.infected ? "rgb(100, 0, 0)" : "#fff",
+  };
+
+  const getGender = () => {
+    if (person.infected) {
+      return <img className="gender-icons" src={dead} alt="Location img" />;
+    } else if (person.gender === "F") {
+      return <img className="gender-icons" src={female} alt="Location img" />;
+    } else if (person.gender === "M") {
+      return <img className="gender-icons" src={male} alt="Location img" />;
+    }
+  };
 
   return (
     <>
       <tbody>
-        <tr
-          key={index + 1}
-          style={{
-            textDecoration: person.infected ? "line-through" : "none",
-            color: person.infected ? "rgb(100, 0, 0)" : "#fff",
-          }}
-        >
-          <th scope="row">{index + 1}</th>
-          <td>{person.name}</td>
-          <td>{person.gender}</td>
-          <td>{person.age}</td>
-          <td>{person.lonlat}</td>
+        <tr key={index + 1} style={tableStyle}>
+          <th scope="row">{getGender()}</th>
+          <td className="pt-3">{person.name}</td>
+          <td className="pt-3">{person.age}</td>
+          <td className="pt-3">{person.lonlat}</td>
           <td>
             <ReportInfectedModal name={person.name} id={person.id} />
           </td>
@@ -59,11 +71,11 @@ function People() {
       <Table hover borderless className="text-center text-white">
         <thead>
           <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Gender</th>
-            <th>Location</th>
+            <th>PERSON</th>
+            <th>NAME</th>
+            <th>AGE</th>
+            <th>LOCATION</th>
+            <th>REPORT</th>
           </tr>
         </thead>
         {people.map((person, index) => (
