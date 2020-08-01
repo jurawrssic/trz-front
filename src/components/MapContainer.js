a1b237e5c9511bf5bee283788ae1ec527dfa8a3c
Julia Rodrigues <juliardgs@yahoo.com.br>
2020-08-01 17:20:48 -0300

    Map now has Markers which sets the Persons lonlat

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,19 +1,40 @@
-import React from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import React, { useState } from "react";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
-function MapContainer() {
+function MapContainer({ onMarkerChange }) {
   const mapStyles = {
-    width: "100%",
-    height: "100%",
+    minWidth: "35rem",
+    minHeight: "20rem",
+  };
+
+  const [lat, setLat] = useState(47.444);
+  const [lng, setLng] = useState(-122.176);
+
+  const setMarker = (t, map, coord) => {
+    const { latLng } = coord;
+    const lat = latLng.lat();
+    const lng = latLng.lng();
+
+    setLat(lat);
+    setLng(lng);
+
+    var string = "POINT (" + lng + " " + lat + ")";
+
+    onMarkerChange(string);
   };
 
   return (
-    <Map
-      google={this.props.google}
-      zoom={8}
-      style={mapStyles}
-      initialCenter={{ lat: 47.444, lng: -122.176 }}
-    />
+    <div id="divMap" className="container mt-4" style={mapStyles}>
+      <Map
+        google={window.google}
+        zoom={8}
+        style={mapStyles}
+        initialCenter={{ lat: 47.444, lng: -122.176 }}
+        onClick={setMarker}
+      >
+        <Marker position={{ lat, lng }} />
+      </Map>
+    </div>
   );
 }
 
