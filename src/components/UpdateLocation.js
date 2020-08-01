b155367282d37dabc30e951fe5cc1390d3ac9169
Julia Rodrigues <juliardgs@yahoo.com.br>
2020-08-01 01:07:32 -0300

    Testing

--- src/components/UpdateLocation.js
+++ src/components/UpdateLocation.js
@@ -1,7 +1,4 @@
 import React from "react";
-import axios from "axios";
-import Map from "./Map";
-import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 import "./../App.css";
 
