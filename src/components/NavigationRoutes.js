897dc1610ca09e726b314aca9ee30867b09a1b3a
Julia Rodrigues <juliardgs@yahoo.com.br>
2020-08-02 06:44:24 -0300

    Defined exact path on homepage

--- src/components/NavigationRoutes.js
+++ src/components/NavigationRoutes.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import RegisteredPeople from "./RegisteredPeople";
+import Informations from "./Informations";
 import Register from "./Register";
 import UpdateLocation from "./UpdateLocation";
 import Trade from "./Trade";
+import RegisteredPeople from "./RegisteredPeople";
 
 function NavigationRoutes() {
   return (
     <>
+      <Route exact path="/" component={Informations}></Route>
       <Route path="/register" component={Register}></Route>
       <Route path="/updateLocation" component={UpdateLocation}></Route>
       <Route path="/trade" component={Trade}></Route>
