b767b4576bc4b40be746efeab623e57f25cdabc5
Julia Rodrigues <juliardgs@yahoo.com.br>
2020-08-02 06:43:31 -0300

    Changing view in progress

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,57 +1,82 @@
 import React from "react";
 import { Col, Row, FormGroup, Label, Input } from "reactstrap";
-import water from "./../assets/water.png";
-import soup from "./../assets/food.png";
-import meds from "./../assets/aid.png";
-import weapon from "./../assets/gun.png";
 
-function Inventory() {
+import waterIcon from "./../assets/water.png";
+import soupIcon from "./../assets/food.png";
+import medsIcon from "./../assets/aid.png";
+import weaponIcon from "./../assets/gun.png";
+
+function Inventory({ onSubmitInventory, currentInventory }) {
+  const onChangeWater = (e) => {
+    const newInventory = { ...currentInventory, water: e.target.value };
+    onSubmitInventory(newInventory);
+  };
+
+  const onChangeSoup = (e) => {
+    const newInventory = { ...currentInventory, soup: e.target.value };
+    onSubmitInventory(newInventory);
+  };
+
+  const onChangeMeds = (e) => {
+    const newInventory = { ...currentInventory, meds: e.target.value };
+    onSubmitInventory(newInventory);
+  };
+
+  const onChangeWeapon = (e) => {
+    const newInventory = { ...currentInventory, weapon: e.target.value };
+    onSubmitInventory(newInventory);
+  };
+
   return (
     <FormGroup>
-      <Row className="justify-content-center text-center border-top pt-3">
+      <Row className="justify-content-center text-center">
         <Col xs="3">
-          <img src={water} alt="Water" />
+          <img src={waterIcon} alt="Water" />
           <Label for="inventoryWater">Fiji Water:</Label>
           <Input
             type="number"
-            name="water"
+            name="inventoryWater"
             id="inventoryWater"
             placeholder="Enter ammount..."
+            onChange={onChangeWater}
           />
         </Col>
         <Col xs="3">
-          <img src={soup} alt="Soup" />
+          <img src={soupIcon} alt="Soup" />
           <Label for="inventorySoup">Campbell Soup:</Label>
           <Input
             type="number"
-            name="soup"
+            name="inventorySoup"
             id="inventorySoup"
             placeholder="Enter ammount..."
+            onChange={onChangeSoup}
           />
         </Col>
         <Col xs="3">
-          <img src={meds} alt="Aid" />
+          <img src={medsIcon} alt="Aid" />
           <Label for="inventoryMeds">First Aid Pouch:</Label>
           <Input
             type="number"
-            name="meds"
+            name="inventoryMeds"
             id="inventoryMeds"
             placeholder="Enter ammount..."
+            onChange={onChangeMeds}
           />
         </Col>
         <Col xs="3">
           <img
             className="d-block"
             style={{ margin: "0 auto" }}
-            src={weapon}
+            src={weaponIcon}
             alt="Gun"
           />
-          <Label for="inventoryWeapons">AK47:</Label>
+          <Label for="inventoryWeapon">AK47:</Label>
           <Input
             type="number"
-            name="weapons"
-            id="inventoryWeapons"
+            name="inventoryWeapon"
+            id="inventoryWeapon"
             placeholder="Enter ammount..."
+            onChange={onChangeWeapon}
           />
         </Col>
       </Row>
