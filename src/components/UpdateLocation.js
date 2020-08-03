6ab321bfbb7e2f75342e268edd412b0e11a9f702
Julia Rodrigues <juliardgs@yahoo.com.br>
2020-08-03 01:47:22 -0300

    You can now update a persons location

--- src/components/UpdateLocation.js
+++ src/components/UpdateLocation.js
@@ -1,73 +1,73 @@
 import React, { useState } from "react";
-import { Button, Row, Col, Form, Label, Input } from "reactstrap";
-import { useForm } from "react-hook-form";
+import { Button, Row, Col, Form, Input } from "reactstrap";
+
+import axios from "axios";
 
 import MapContainer from "./MapContainer";
 import RegisteredPeople from "./RegisteredPeople";
 
 function UpdateLocation() {
   const [lonlat, setLonlat] = useState("");
-  const onMarkerChange = (lonlat) => {
-    setLonlat(lonlat);
+  const onMarkerChange = (newLonlat) => {
+    setLonlat(newLonlat);
   };
 
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => {
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ infected: data.reportedPersonID }),
-    };
+  const [selectedPerson, setSelectedPerson] = useState({});
+  const onSelectPerson = (newlySelectedPerson) => {
+    setSelectedPerson(newlySelectedPerson);
 
-    fetch(
-      "http://zssn-backend-example.herokuapp.com/api/people/" +
-        data.reportedBy +
-        "/report_infection.json",
-      requestOptions
-    )
-      .then((response) => console.log(response))
-      .then((data) => console.log(data));
+  }
+
+  const onSubmit = () => {
+    const id = selectedPerson.location.substring(53);
+    console.log(id);
+    const url = axios.patch('http://zssn-backend-example.herokuapp.com/api/people/' + id + '.json',
+      {
+        person: {
+          name: selectedPerson.name,
+          age: selectedPerson.age,
+          gender: selectedPerson.gender,
+          lonlat
+        }
+      }
+    ).then(response => alert(response.status))
+      .catch(error => alert(error))
+
+    console.log(url);
   };
 
   return (
     <>
-      <Form
-        id="updateLocationForm"
-        name="updateLocationForm"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h4>New Location</h4>
-        <Row className="justify-content-center border-top pt-4">
-          <Col xs="8">
-            <RegisteredPeople />
-          </Col>
-          <Input
-            hidden
-            id="personLonLat"
-            name="personLonLat"
-            defaultValue={lonlat}
-            innerRef={register}
-          ></Input>
-        </Row>
-        <Row>
-          <Col xs="12">
-            <MapContainer onMarkerChange={onMarkerChange} />
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col xs="12">
-            <Button
-              outline
-              color="secondary"
-              form="updateLocationForm"
-              type="submit"
-              className="mt-4"
-            >
-              Change my Location!
+      <h4>New Location</h4>
+      <Row className="justify-content-center border-top pt-4">
+        <Col xs="8">
+          <RegisteredPeople screenLocation={"outsideReportComponent"} onSelectPerson={onSelectPerson} />
+        </Col>
+        <Input
+          hidden
+          id="personLonLat"
+          name="personLonLat"
+          defaultValue={lonlat}
+        ></Input>
+      </Row>
+      <Row>
+        <Col xs="12">
+          <MapContainer onMarkerChange={onMarkerChange} />
+        </Col>
+      </Row>
+      <Row className="text-center">
+        <Col xs="12">
+          <Button
+            outline
+            color="secondary"
+            form="updateLocationForm"
+            className="mt-4"
+            onClick={onSubmit}
+          >
+            Change my Location!
             </Button>
-          </Col>
-        </Row>
-      </Form>
+        </Col>
+      </Row>
     </>
   );
 }
